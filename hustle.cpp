cc9dee37752c14ad282919fcec3261af55deac6d
Pratham Jain <83359099+iprathamjain@users.noreply.github.com>
2022-10-05 18:35:06 +0530

    added code for initial template

---
+++ hustle.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// typedef long long ll;
+// typedef long double ld;
+ 
+// typedef pair<int, int> pi;
+// typedef pair<ll,ll> pl;
+// typedef pair<ld,ld> pd;
+ 
+// typedef vector<int> vi;
+// typedef vector<ld> vd;
+// typedef vector<ll> vl;
+// typedef vector<pi> vpi;
+// typedef vector<pl> vpl;
+// // typedef vector<cd> vcd;
+
+// #define FOR(i, a, b) for (int i=a; i<(b); i++)
+// #define F0R(i, a) for (int i=0; i<(a); i++)
+// #define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+// #define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+// #define trav(a,x) for (auto& a : x)
+
+// #define sz(x) (int)(x).size()
+// // #define mp make_pair
+// #define pb push_back
+// #define f first
+// #define s second
+// #define lb lower_bound
+// #define ub upper_bound
+// #define all(x) x.begin(), x.end()
+// #define ins insert
+// #define yes cout<<"YES"<<endl
+// #define no cout<<"NO"<<endl
+// #define in int n;cin>>n
+
+#define int long long int
+// #define mod 1000000007    
+// #define mod1 998244353
+// #define MX 100001
+
+
+void Hustle(){ 
+#ifndef ONLINE_JUDGE 
+   freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout); 
+#endif
+}
+
+int gcd(int a, int b){if(b==0)return a;return gcd(b,a%b);}
+bool prime(int n){for(int i=2;i*i<=n;i++){if(n%i==0)return false;}return true;}
+void arrin(int arr[], int n){for(int i=1;i<=n;i++)cin>>arr[i];}
+
+
+//////////////////////////////////////////////////////////////////////
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> v;
+    for(auto& )
+}
+
+
+ 
+
+signed main()
+{  
+   cin.tie(0)->sync_with_stdio(0); 
+   cin.exceptions(cin.failbit);
+   Hustle();
+   // int t=0;
+   // cin>>t;
+   // while(t--){
+   //    solve();
+   // }   
+   solve();
+
+   return 0;
+}
+
+
+
